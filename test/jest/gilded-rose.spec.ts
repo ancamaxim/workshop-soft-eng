61193a38e7b1902dd61f4070b70e3aaa298ecce4
Anca Maxim <ancastef7@gmail.com>
2024-11-09 13:02:49 +0200

    Updated tests

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -9,7 +9,7 @@ describe('Gilded Rose', () => {
         const items = gildedRose.updateQuality();
 
         // Assert
-        expect(items[0].name).toBe('bar');
+        expect(items[0].name).toBe('foo');
     });
 
     it('sword quality drops by 1', () => {
@@ -20,6 +20,73 @@ describe('Gilded Rose', () => {
       const items = gildedRose.updateQuality();
 
       // Assert
-      expect(items[0].quality).toBe(1);
+      expect(items[0].quality).toBe(0);
     })
+
+  it('Aged Brie should increase by 1', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 20)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(21);
+  })
+
+  it('Aged Brie should increase by 2', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', -1, 30)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(32);
+  })
+
+  it('Sulfuras should stay constant', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 1, 80)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(80);
+  })
+
+  it('Conjured should decrease by 4', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', -1, 10)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(6);
+  })
+
+  it('Backstage should increase by 1', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(50);
+  })
+
+  it('Backstage should increase by 1', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(50);
+  })
+
 });
